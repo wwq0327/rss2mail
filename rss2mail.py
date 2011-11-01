579f11f9f1f954993fd32ae109d2dc40f53d9c76
wwq0327 <wwq0327@gmail.com>
2011-11-01 20:15:23 +0800

    fix

--- rss2mail.py
+++ rss2mail.py
@@ -14,10 +14,17 @@ import datetime
 import feedparser
 from dateutil import parser, rrule
 
+MAX_DAYS = 7
 blog_addr = "http://sociallearnlab.org/xiuli/?feed=rss2"
 
 def read_blog(entries):
-    '''读取一条日志'''
+    """读取一条日志, 返回字典：
+    
+    :title: 标题
+    :updated: 更新日期
+    :link: 日志链接地址
+    :content: 内容，只取了140个字符。
+    """
 
     blog_info = {}
     if entries:
@@ -29,19 +36,19 @@ def read_blog(entries):
     return blog_info
 
 def store_recoder(blog):
-    """读取单条RSS信息内容，并与限定时间进行比较，
-    如果在限定时间范围之类，则输出。
+    """读取单条RSS信息内容，并与限定时间进行比较。
     """
-    updated_time = blog['updated']
-    blog_time = parser.parse(updated_time)
+    
+    updated = blog['updated']
+    blog_time = parser.parse(updated) ## 解析日期
     my_d = blog_time.strftime("%Y-%m-%d %H")
-    my_time = datetime.datetime.strptime(my_d, "%Y-%m-%d %H")
+    my_time = datetime.datetime.strptime(my_d, "%Y-%m-%d %H") ## 转换成rrule可读日期对象
     
     td_now = datetime.datetime.now()
 
-    days = rrule.rrule(rrule.DAILY, dtstart=my_time, until=td_now).count()
+    days = rrule.rrule(rrule.DAILY, dtstart=my_time, until=td_now).count() ## 获取RSS日期与当前日期所差天数 
 
-    if 0 <= days <= 7:
+    if 0 <= days <= MAX_DAYS:
         return True
     else:
         return False

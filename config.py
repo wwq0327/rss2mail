0f6c7bc1b626f78f3a7c1fdc59bd973faae9c813
wwq0327 <wwq0327@gmail.com>
2011-11-01 20:54:26 +0800

    fix

--- config.py
+++ config.py
@@ -9,12 +9,13 @@
     
     :date: 2011-11-01
     :author: wwq0327 <wwq0327@gmail.com>
-    :license: LGPL 2 or later (see README/COPYING/LICENSE)
+    :license: GPL or later (see README/COPYING/LICENSE)
 
 """
-# 日志地址列表
+# 日志地址列表, 可自行添加博客的RSS地址，暂只在wordpress日志系统上测试通过
 BLOG_ADDR_LIST = ["http://sociallearnlab.org/xiuli/?feed=rss2",
-             "http://linuxtoy.org/?feed=rss2"]
+                  "http://linuxtoy.org/?feed=rss2",
+                  ]
              
 # 读取日志的最大限制大数
 MAX_DT_DAYS = 7 # 7, 表示读当前日期7天前的记录，含当前日期
